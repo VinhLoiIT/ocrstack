286f4b5d8bd06a7d8fe1316d9508b88b3cf6ca3d
Loi Ly <vinhloiit1327@gmail.com>
2021-05-04 20:36:34 +0700

    add CSVDataset

--- ocrstack/data/dataset.py
+++ ocrstack/data/dataset.py
@@ -10,6 +10,8 @@ from torch.utils.data import Dataset
 
 __all__ = [
     'OCRDataset',
+    'CSVDataset',
+    'DummyDataset',
     'glob'
 ]
 
@@ -30,6 +32,7 @@ class OCRDataset(Dataset):
                  text_file_suffix: str = 'txt',
                  encoding: str = 'utf8'
                  ):
+        super(OCRDataset, self).__init__()
         self.logger = logging.getLogger('OCRDataset')
         if isinstance(image_paths, list):
             assert len(image_paths) > 0, "Image Paths should not be empty"
@@ -73,7 +76,57 @@ class OCRDataset(Dataset):
         return data
 
 
+class CSVDataset(Dataset):
+    def __init__(self,
+                 image_dir: Union[Path, str],
+                 csv_path: Union[Path, str],
+                 image_transform: Optional[Callable] = None,
+                 text_transform: Optional[Callable] = None,
+                 delimiter=',',
+                 encoding: str = 'utf8'):
+        super(CSVDataset, self).__init__()
+        self.csv_path = csv_path
+        self.image_dir = Path(image_dir)
+        self.image_transform = image_transform
+        self.text_transform = text_transform
+        import csv
+        with open(csv_path, 'rt', encoding=encoding) as f:
+            reader = csv.reader(f, delimiter=delimiter)
+            self.rows = [row for row in reader]
+
+    def __len__(self) -> int:
+        return len(self.rows)
+
+    def __getitem__(self, idx):
+        image_name, raw_text = self.rows[idx]
+
+        image_path = self.image_dir.joinpath(image_name)
+        image = Image.open(image_path).convert('RGB')
+        if self.image_transform is not None:
+            image = self.image_transform(image)
+
+        if self.text_transform is not None:
+            text = self.text_transform(raw_text)
+
+        data = {
+            'metadata': {
+                'index': idx,
+                'imagePath': str(image_path),
+                'rawText': raw_text,
+            },
+            'image': image,
+            'text': text,
+        }
+
+        return data
+
+
 class DummyDataset(Dataset):
+
+    '''
+    This class provides a dummy data set for debugging and testing flow.
+    '''
+
     def __init__(self, num_samples, image_channels, height, width, max_lengths, vocab_size, seq2seq=False):
         super(DummyDataset, self).__init__()
         self.images = torch.rand(num_samples, image_channels, height, width)
