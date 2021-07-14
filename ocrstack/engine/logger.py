97b7830bcf2e03149a97a484ad903cdcf9236428
Loi Ly <vinhloiit1327@gmail.com>
2021-07-14 18:26:41 +0700

    add log_scalars for logging multiple scalars at once; refactor logging format

--- ocrstack/engine/logger.py
+++ ocrstack/engine/logger.py
@@ -1,6 +1,6 @@
 import logging
-import sys
-from typing import List, Optional
+from io import StringIO
+from typing import Dict, List, Optional
 
 from torch.utils.tensorboard.writer import SummaryWriter
 
@@ -16,30 +16,43 @@ class LoggerInterface:
     def log_scalar(self, name: str, value: float, step: Optional[int] = None):
         raise NotImplementedError()
 
+    def log_scalars(self, name: str, scalar_dict: Dict[str, float], step: Optional[int] = None):
+        raise NotImplementedError()
+
 
 class NoLogger(LoggerInterface):
 
     def log_scalar(self, name: str, value: float, step: Optional[int] = None):
         pass
 
+    def log_scalars(self, name: str, scalar_dict: Dict[str, float], step: Optional[int] = None):
+        pass
+
 
 class ConsoleLogger(LoggerInterface):
     def __init__(self, log_interval: int, name: Optional[str] = None):
         self.logger = logging.getLogger(name or self.__name__)
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        self.logger.addHandler(handler)
         self.log_interval = log_interval
         self._internal_step = 0
 
     def open(self):
+        logging.basicConfig(format='[%(levelname)s] %(name)s: %(message)s', level=logging.INFO)
         self._internal_step = 0
 
-    def log_scalar(self, name: str, value: float, step: Optional[int] = None):
+    def _log(self, string: str, step):
         self._internal_step = step or self._internal_step
         if self._internal_step % self.log_interval == 0:
-            self.logger.info(f'Step {self._internal_step:5d}: {name} = {value:.04f}')
+            self.logger.info(string)
         self._internal_step += 1
+
+    def log_scalar(self, name: str, value: float, step: Optional[int] = None):
+        self._log(f'Step {self._internal_step:5d}: {name} = {value:.04f}', step)
+
+    def log_scalars(self, name: str, scalar_dict: Dict[str, float], step: Optional[int] = None):
+        with StringIO() as writer:
+            writer.write(f'Step {self._internal_step:5d}: ')
+            writer.write(' - '.join([f'{name}: {value:.04f}' for name, value in scalar_dict.items()]))
+            self._log(writer.getvalue(), step)
 
 
 class TensorboardLogger(LoggerInterface):
@@ -59,6 +72,9 @@ class TensorboardLogger(LoggerInterface):
     def log_scalar(self, name: str, value: float, step: Optional[int] = None):
         self.logger.add_scalar(name, value, step)
 
+    def log_scalars(self, name: str, scalar_dict: Dict[str, float], step: Optional[int] = None):
+        self.logger.add_scalars(name, scalar_dict, step)
+
 
 class ComposeLogger(LoggerInterface):
 
@@ -76,3 +92,7 @@ class ComposeLogger(LoggerInterface):
     def log_scalar(self, name: str, value: float, step: Optional[int]):
         for logger in self.loggers:
             logger.log_scalar(name, value, step=step)
+
+    def log_scalars(self, name: str, scalar_dict: Dict[str, float], step: Optional[int] = None):
+        for logger in self.loggers:
+            logger.log_scalars(name, scalar_dict, step=step)
