92d62d4dccff08d7ce77e173dbaab43b8d58dbb1
Loi Ly <vinhloiit1327@gmail.com>
2021-07-21 11:58:35 +0700

    move generate padding to utils

--- ocrstack/models/layers/sequence_decoder.py
+++ ocrstack/models/layers/sequence_decoder.py
@@ -178,12 +178,3 @@ class AttentionLSTMDecoder(BaseDecoder):
                 break
         outputs = torch.stack(outputs, dim=1)
         return outputs, lengths
-
-
-def _generate_square_subsequent_mask(sz: int) -> Tensor:
-    r"""Generate a square mask for the sequence. The masked positions are filled with float('-inf').
-        Unmasked positions are filled with float(0.0).
-    """
-    mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-    return mask
--- ocrstack/models/utils.py
+++ ocrstack/models/utils.py
@@ -5,3 +5,12 @@ def generate_padding_mask_from_lengths(lengths: torch.Tensor) -> torch.Tensor:
     B, S = len(lengths), lengths.max()
     padding_mask = torch.arange(0, S, device=lengths.device).expand(B, S) >= lengths.unsqueeze(-1)
     return padding_mask
+
+
+def generate_square_subsequent_mask(sz: int) -> torch.Tensor:
+    r"""Generate a square mask for the sequence. The masked positions are filled with float('-inf').
+        Unmasked positions are filled with float(0.0).
+    """
+    mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+    return mask
