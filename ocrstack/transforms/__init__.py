d9f693e22d0eac8a88a71f5ff850a101f04db991
Loi Ly <vinhloiit1327@gmail.com>
2021-07-24 17:01:01 +0700

    fix transforms: - fix missing import in __init__

--- ocrstack/transforms/__init__.py
+++ ocrstack/transforms/__init__.py
@@ -1,2 +1,2 @@
-from .image import ScaleHeight, ImageTransform, BatchPadImages
+from .image import ScaleHeight, BatchPadImages
 from .string import LabelDecoder, Replace, TextToTensor, ToCharList, OneHotEncoding, AddSeq2SeqTokens, BatchPadTexts
