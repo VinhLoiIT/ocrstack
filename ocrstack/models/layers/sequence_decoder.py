9392a510d77a36171b5f26416254252e8d9463a9
Loi Ly <vinhloiit1327@gmail.com>
2021-07-28 08:49:02 +0700

    fix models: fix sharing weights

--- ocrstack/models/layers/sequence_decoder.py
+++ ocrstack/models/layers/sequence_decoder.py
@@ -42,14 +42,13 @@ class BaseDecoder(nn.Module):
                               cfg.MODEL.TEXT_EMBED.VOCAB_SIZE,
                               bias=cfg.MODEL.TEXT_EMBED.OUT_BIAS)
 
-        shared_weight = None
-        if cfg.MODEL.TEXT_EMBED.SHARE_WEIGHT_IN_OUT:
-            shared_weight = out_embed.weight
-
         in_embed = nn.Embedding(cfg.MODEL.TEXT_EMBED.VOCAB_SIZE,
                                 cfg.MODEL.TEXT_EMBED.EMBED_SIZE,
-                                cfg.MODEL.TEXT_EMBED.PAD_IDX,
-                                _weight=shared_weight)
+                                cfg.MODEL.TEXT_EMBED.PAD_IDX)
+
+        if cfg.MODEL.TEXT_EMBED.SHARE_WEIGHT_IN_OUT:
+            in_embed.weight = out_embed.weight
+
         return in_embed, out_embed
 
 
