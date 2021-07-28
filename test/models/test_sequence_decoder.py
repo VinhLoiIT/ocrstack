0a96ed99df1b3c7db981b10dd30b841f4d67995c
Loi Ly <vinhloiit1327@gmail.com>
2021-07-28 09:31:11 +0700

    feat models: add test positional encoding decoder

--- test/models/test_sequence_decoder.py
+++ test/models/test_sequence_decoder.py
@@ -32,6 +32,35 @@ def test_transformer_decoder_adapter_decode():
     assert output.shape == torch.Size([B, T, cfg.MODEL.TEXT_EMBED.VOCAB_SIZE])
 
 
+def test_transformer_decoder_adapter_pe():
+    cfg = Config()
+    cfg.MODEL.DECODER.TYPE = 'tf_decoder'
+    cfg.MODEL.DECODER.D_MODEL = 10
+    cfg.MODEL.DECODER.NUM_HEADS = 5
+    cfg.MODEL.DECODER.NUM_LAYERS = 2
+    cfg.MODEL.DECODER.MAX_LENGTH = 10
+
+    cfg.MODEL.TEXT_EMBED.POS_ENC_TYPE == 'sinusoidal'
+    cfg.MODEL.TEXT_EMBED.EMBED_SIZE = 10
+    cfg.MODEL.TEXT_EMBED.VOCAB_SIZE = 5
+    cfg.MODEL.TEXT_EMBED.SOS_IDX = 0
+    cfg.MODEL.TEXT_EMBED.EOS_IDX = 1
+    cfg.MODEL.TEXT_EMBED.PAD_IDX = 2
+    cfg.MODEL.TEXT_EMBED.OUT_BIAS = False
+    cfg.MODEL.TEXT_EMBED.SHARE_WEIGHT_IN_OUT = True
+
+    decoder = TransformerDecoderAdapter(cfg)
+
+    B, T, S = 2, 4, 8
+    src = torch.rand(B, S, cfg.MODEL.DECODER.D_MODEL)
+    tgt = torch.randint(cfg.MODEL.TEXT_EMBED.VOCAB_SIZE, (B, T))
+
+    decoder.eval()
+    output = decoder.decode(src, 10)
+    output = decoder.forward(src, tgt)
+    assert output.shape == torch.Size([B, T, cfg.MODEL.TEXT_EMBED.VOCAB_SIZE])
+
+
 def test_attention_lstm_decoder_forward():
     cfg = Config()
     cfg.MODEL.DECODER.TYPE = 'attn_lstm'
