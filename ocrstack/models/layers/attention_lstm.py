05454b501d29f3ed3fe401c039066c3d32441cb5
Loi Ly <vinhloiit1327@gmail.com>
2021-08-05 22:07:31 +0700

    feat models: add attention gru cell

--- ocrstack/models/layers/attention_lstm.py
+++ ocrstack/models/layers/attention_lstm.py
@@ -10,10 +10,6 @@ from .attention import DotProductAttention
 class AttentionLSTMCell(nn.Module):
     r"""An attention-based long short-term memory (LSTM) cell.
 
-    Note:
-    At the moment, this class uses `DotProductAttention` for attention mechanism. Therefore, `memory_size` is required
-    to equal to `hidden_size`
-
     Shapes:
     -------
     - memory: (B, S, E1)
@@ -24,8 +20,6 @@ class AttentionLSTMCell(nn.Module):
 
     def __init__(self, memory_size, embed_size, hidden_size, num_layers=1, num_heads=1, bias=True):
         super().__init__()
-        assert memory_size == hidden_size
-
         self.lstm_cells = nn.ModuleList()
         first_cell = nn.LSTMCell(hidden_size + embed_size, hidden_size, bias)
         later_cells = [nn.LSTMCell(hidden_size, hidden_size, bias=bias) for _ in range(num_layers - 1)]
@@ -71,3 +65,57 @@ class AttentionLSTMCell(nn.Module):
 
         out = self.out(context)                                                 # B, E
         return out, context, (out_hidden, out_cell)
+
+
+class AttentionGRUCell(nn.Module):
+    r"""An attention-based gated recurrent unit (GRU) cell.
+
+    Shapes:
+    -------
+    - memory: (B, S, E1)
+    - prev_predict: (B, E2)
+    - prev_hidden: (B, num_layers * hidden_size)
+    - memory_key_padding_mask: (B, S)
+    """
+
+    def __init__(self, memory_size, embed_size, hidden_size, num_layers=1, num_heads=1, bias=True):
+        super().__init__()
+        self.gru_cells = nn.ModuleList()
+        first_cell = nn.GRUCell(hidden_size + embed_size, hidden_size, bias)
+        later_cells = [nn.GRUCell(hidden_size, hidden_size, bias=bias) for _ in range(num_layers - 1)]
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.gru_cells.append(first_cell)
+        self.gru_cells.extend(later_cells)
+
+        self.attention = DotProductAttention(scaled=True, embed_dim=hidden_size,
+                                             k_dim=memory_size, v_dim=memory_size, num_heads=num_heads)
+        self.out = nn.Linear(hidden_size, embed_size)
+
+    def forward(self,
+                memory: Tensor,
+                prev_predict: Tensor,
+                prev_attn: Tensor,
+                prev_state: Optional[Tensor] = None,
+                memory_key_padding_mask: Optional[Tensor] = None
+                ) -> Tuple[Tensor, Tensor, Tuple[Tensor, Tensor]]:
+
+        inputs = torch.cat((prev_predict, prev_attn), dim=1)                    # B, E + H
+
+        if prev_state is None:
+            prev_state = torch.zeros(inputs.size(0), self.num_layers * self.hidden_size, device=inputs.device)
+
+        out_hidden = torch.empty_like(prev_state)
+
+        for i, lstm_cell in enumerate(self.gru_cells):
+            in_hidden_i = prev_state[:, i*self.hidden_size:(i+1)*self.hidden_size]
+            out_hidden_i = lstm_cell(inputs, in_hidden_i)
+            out_hidden[:, i*self.hidden_size:(i+1)*self.hidden_size] = out_hidden_i
+            inputs = out_hidden_i
+
+        context = self.attention(out_hidden_i.unsqueeze(1), memory, memory,
+                                 k_padding_mask=memory_key_padding_mask)[0]     # B, 1, E
+        context = context.squeeze(1)                                            # B, H
+
+        out = self.out(context)                                                 # B, E
+        return out, context, out_hidden
--- test/models/test_attention_lstm.py
+++ test/models/test_attention_lstm.py
@@ -1,11 +1,6 @@
 import torch
 import pytest
-from ocrstack.models.layers.attention_lstm import AttentionLSTMCell
-
-
-def test_attention_lstm_cell_assert():
-    with pytest.raises(AssertionError):
-        AttentionLSTMCell(512, 128, 256)
+from ocrstack.models.layers.attention_lstm import AttentionGRUCell, AttentionLSTMCell
 
 
 @pytest.mark.parametrize('num_layers', (1, 2))
@@ -13,7 +8,8 @@ def test_attention_lstm_cell_assert():
 @pytest.mark.parametrize('is_init_prev_state', (True, False))
 def test_attention_lstm_cell(num_layers, batch_size, is_init_prev_state):
 
-    memory_size = hidden_size = 15
+    memory_size = 8
+    hidden_size = 15
     embed_size = 10
     memory_length = 5
 
@@ -32,9 +28,40 @@ def test_attention_lstm_cell(num_layers, batch_size, is_init_prev_state):
                                                   prev_state, memory_key_padding_mask)
 
     assert out.shape == torch.Size((batch_size, embed_size))
-    assert context.shape == torch.Size((batch_size, memory_size))
+    assert context.shape == torch.Size((batch_size, hidden_size))
     assert hidden.shape == torch.Size((batch_size, num_layers * hidden_size))
     assert cell.shape == torch.Size((batch_size, num_layers * hidden_size))
+
+    # test backward compatible
+    out.mean().backward()
+
+
+@pytest.mark.parametrize('num_layers', (1, 2))
+@pytest.mark.parametrize('batch_size', (1, 2))
+@pytest.mark.parametrize('is_init_prev_state', (True, False))
+def test_attention_gru_cell(num_layers, batch_size, is_init_prev_state):
+
+    memory_size = 8
+    hidden_size = 15
+    embed_size = 10
+    memory_length = 5
+
+    prev_state = None
+    if is_init_prev_state:
+        prev_state = torch.rand(batch_size, num_layers * hidden_size)
+
+    attn_lstm_cell = AttentionGRUCell(memory_size, embed_size, hidden_size, num_layers)
+    memory = torch.rand(batch_size, memory_length, memory_size)
+    prev_predict = torch.rand(batch_size, embed_size)
+    prev_attn = torch.rand(batch_size, hidden_size)
+    memory_key_padding_mask = None
+
+    out, context, hidden = attn_lstm_cell(memory, prev_predict, prev_attn,
+                                          prev_state, memory_key_padding_mask)
+
+    assert out.shape == torch.Size((batch_size, embed_size))
+    assert context.shape == torch.Size((batch_size, hidden_size))
+    assert hidden.shape == torch.Size((batch_size, num_layers * hidden_size))
 
     # test backward compatible
     out.mean().backward()
