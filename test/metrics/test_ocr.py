d62f76e27e1cf38bd312a99ae79f136a88dd21a6
Loi Ly <vinhloiit1327@gmail.com>
2021-08-16 07:20:09 +0700

    update test for Norm(CER/WER)Meter, Global(CER,WER)Meter

--- test/metrics/test_ocr.py
+++ test/metrics/test_ocr.py
@@ -1,6 +1,8 @@
-from ocrstack.metrics.ocr import (compute_acc, compute_global_cer,
-                                  compute_global_wer, compute_norm_cer,
-                                  compute_norm_wer, split_by_token)
+from ocrstack.metrics.ocr import (ACCMeter, GlobalCERMeter, GlobalWERMeter,
+                                  NormCERMeter, NormWERMeter, compute_acc,
+                                  compute_global_cer, compute_global_wer,
+                                  compute_norm_cer, compute_norm_wer,
+                                  split_by_token)
 
 
 def test_split_by_token_normal():
@@ -50,3 +52,53 @@ def test_compute_acc():
     assert compute_acc(['a', ' ', 'b', 'c'], ['a', 'b', 'c']) == 0
     assert compute_acc(['a', ' ', 'b', ' ', 'c'], ['c', 'b', 'a']) == 0
     assert compute_acc(['a', ' ', 'b', ' ', 'c'], ['a', ' ', 'b', ' ', 'c']) == 1
+
+
+def test_acc_meter():
+    meter = ACCMeter()
+    meter.update([['a', ' ', 'b', 'c']],
+                 [['a', ' ', 'b', 'c']])
+    assert meter.compute() == 1 / 1
+    meter.update([['a', ' ', 'b', 'c'], ['d', 'e', 'f']],
+                 [['a', ' ', 'b', 'c'], ['d', 'e']])
+    assert meter.compute() == 2 / 3
+
+
+def test_global_cer_meter():
+    meter = GlobalCERMeter()
+    meter.update([['a', ' ', 'b', 'c']],
+                 [['a', ' ', 'b', 'c']])
+    assert meter.compute() == 0
+    meter.update([['a', ' ', 'b', 'c'], ['d', 'e', 'f']],
+                 [['a', ' ', 'b', 'c'], ['d', 'e']])
+    assert meter.compute() == 1 / 10
+
+
+def test_norm_cer_meter():
+    meter = NormCERMeter()
+    meter.update([['a', ' ', 'b', 'c']],
+                 [['a', ' ', 'b', 'c']])
+    assert meter.compute() == 0
+    meter.update([['a', ' ', 'b', 'c'], ['d', 'e', 'f']],
+                 [['a', ' ', 'b', 'c'], ['d', 'e']])
+    assert meter.compute() == (1/2) / 3
+
+
+def test_global_wer_meter():
+    meter = GlobalWERMeter()
+    meter.update([['a', ' ', 'b', 'c']],
+                 [['a', ' ', 'b', 'c']])
+    assert meter.compute() == 0
+    meter.update([['a', ' ', 'b', 'c'], ['d', 'e', 'f', ' ', 'j']],
+                 [['a', ' ', 'b', 'c'], ['d', 'e']])
+    assert meter.compute() == 2 / 5
+
+
+def test_norm_wer_meter():
+    meter = NormWERMeter()
+    meter.update([['a', ' ', 'b', 'c']],
+                 [['a', ' ', 'b', 'c']])
+    assert meter.compute() == 0
+    meter.update([['a', ' ', 'b', 'c'], ['d', 'e', 'f', ' ', 'j']],
+                 [['a', ' ', 'b', 'c'], ['d', 'e']])
+    assert meter.compute() == (2 / 1) / 3
