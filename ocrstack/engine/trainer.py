90d6a3dfbdb252730961056f68cd9185b4089845
Loi Ly <vinhloiit1327@gmail.com>
2021-08-18 12:12:06 +0700

    refactor Trainer

--- ocrstack/engine/trainer.py
+++ ocrstack/engine/trainer.py
@@ -6,7 +6,7 @@ from typing import Dict, Optional, Tuple, Union
 
 import torch
 from ocrstack.data.collate import Batch
-from ocrstack.data.vocab import ITranslator
+from ocrstack.data.vocab import Seq2SeqVocab
 from ocrstack.metrics.metric import AverageMeter
 from ocrstack.metrics.ocr import (ACCMeter, GlobalCERMeter, GlobalWERMeter,
                                   NormCERMeter, NormWERMeter)
@@ -14,19 +14,18 @@ from ocrstack.models.base import ITrainableS2S
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
+from .utils import set_seed
+
 __all__ = [
-    'S2STrainConfig',
-    'validate_s2s',
-    'visualize_s2s',
-    'train_s2s',
-    'train_s2s_epoch',
+    'S2STrainCfg',
+    'S2STrainer',
     'setup_logging',
     'create_session_dir',
 ]
 
 
 @dataclass()
-class S2STrainConfig:
+class S2STrainCfg:
     n_epochs: int = 1000
     learning_rate: int = 1e-4
     batch_size: int = 2
@@ -39,269 +38,225 @@ class S2STrainConfig:
     save_by: Optional[str] = 'val_loss'
     save_top_k: int = 3
     log_dir: str = 'runs'
-    seed: int = 0
+    seed: Optional[int] = None
+    reduction_char_visualize: Optional[str] = None
 
 
-def _train_s2s_iteration(model: ITrainableS2S, optimizer: torch.optim.Optimizer, batch: Batch) -> float:
-    optimizer.zero_grad()
-    loss = model.forward_batch(batch)
-    loss.backward()
-    optimizer.step()
-    return loss.item()
-
-
-@torch.no_grad()
-def visualize_s2s(cfg: S2STrainConfig,
-                  epoch: int,
-                  model: ITrainableS2S,
-                  translator: ITranslator,
-                  val_loader: DataLoader):
-    logger = logging.getLogger('Visualizing')
-
-    if model.training:
-        model.eval()
-
-    num_iter = len(val_loader) if cfg.num_iter_visualize < 0 else cfg.num_iter_visualize
-    if isinstance(num_iter, float):
-        assert 0 <= num_iter <= 1
-        num_iter = int(num_iter * len(val_loader))
-
-    logger.info('Visualize Epoch [{:3d}]'.format(epoch))
-
-    batch: Batch
-    for i, batch in enumerate(val_loader):
-        if i == num_iter:
-            break
-        batch = batch.to(cfg.device)
-        predicts = model.decode_greedy(batch.images, cfg.max_length, batch.image_mask)[0]
-        predict_strs = translator.translate(predicts)
-
-        for predict_str, tgt in zip(predict_strs, batch.text_str):
-            logger.info(f'Predict: {predict_str} ; Target: {tgt}')
-
-
-@torch.no_grad()
-def validate_s2s(cfg: S2STrainConfig,
-                 epoch: int,
+class S2STrainer:
+    def __init__(self,
+                 cfg: S2STrainCfg,
+                 vocab: Seq2SeqVocab,
                  model: ITrainableS2S,
-                 translator: ITranslator,
+                 optimizer: torch.optim.Optimizer,
+                 train_loader: DataLoader,
                  val_loader: DataLoader,
-                 tb_writer: Optional[SummaryWriter] = None) -> Tuple[float, Dict[str, float]]:
-    logger = logging.getLogger('Validation')
+                 lr_scheduler: Optional[torch.optim.lr_scheduler._LRScheduler] = None,
+                 is_debug: bool = False):
+        self.cfg = cfg
+        self.model = model
+        self.optimizer = optimizer
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.vocab = vocab
+        self.lr_scheduler = lr_scheduler
+        self.is_debug = is_debug
 
-    total_loss = AverageMeter()
-    metrics = {
-        'CER': GlobalCERMeter(),
-        'NormCER': NormCERMeter(),
-        'WER': GlobalWERMeter(),
-        'NormWER': NormWERMeter(),
-        'ACC': ACCMeter(),
-    }  # type: Dict[str, AverageMeter]
+        setup_logging()
+        self.logger = logging.getLogger('Trainer')
 
-    if model.training:
-        model.eval()
+    def state_dict(self, epoch):
+        state_dict = {}
+        state_dict['model'] = self.model.state_dict()
+        state_dict['optimizer'] = self.optimizer.state_dict()
+        if self.lr_scheduler is not None:
+            state_dict['lr_scheduler'] = self.lr_scheduler.state_dict()
+        state_dict['epoch'] = epoch
+        return state_dict
 
-    num_iter = len(val_loader)
-    log_interval = cfg.log_interval
-    if isinstance(log_interval, float):
-        assert 0 <= log_interval <= 1
-        log_interval = int(log_interval * num_iter)
+    def train(self):
+        best_loss = float('inf')
+        best_metric = 0.0
+        self.model.train()
+        self.model.to(self.cfg.device)
 
-    batch: Batch
-    for i, batch in enumerate(val_loader):
-        batch = batch.to(cfg.device)
-        loss = model.forward_batch(batch)
-        predicts = model.decode_greedy(batch.images, cfg.max_length, batch.image_mask)[0]
+        session_dir = Path(create_session_dir(self.cfg.log_dir))
 
-        pred_tokens = translator.translate(predicts)
-        tgt_tokens = translator.translate(batch.text)
+        tensorboard_dir = session_dir.joinpath('tb_logs')
+        tb_writer = SummaryWriter(tensorboard_dir)
 
-        total_loss.add(loss.item() * len(batch), len(batch))
-        for metric in metrics.values():
-            metric.update(pred_tokens, tgt_tokens)
+        ckpt_dir = session_dir.joinpath('ckpt')
+        ckpt_dir.mkdir(parents=True)
+        ckpt_history: queue.Queue = queue.Queue(self.cfg.save_top_k)
 
-        if (i + 1) % log_interval == 0:
-            logger.info('Epoch [{:3d}] - [{:6.2f}%] val_loss = {:.4f} - {}'.format(
-                epoch,
-                (i + 1) * 100 / num_iter,
-                total_loss.compute(),
-                ' - '.join([f'{k}: {v.compute():.4f}' for k, v in metrics.items()])
-            ))
+        num_iter = len(self.train_loader)
+        log_interval = _normalize_interval(self.train_loader, self.cfg.log_interval)
 
-    val_loss = total_loss.compute()
-    out_metrics = {k: v.compute() for k, v in metrics.items()}
+        self.logger.info('Start training')
 
-    if tb_writer is not None:
+        if self.cfg.seed is not None:
+            set_seed(self.cfg.seed)
+
+        for epoch in range(self.cfg.n_epochs):
+            total_loss_meter = AverageMeter()
+            running_loss_meter = AverageMeter()
+
+            if not self.model.training:
+                self.model.train()
+
+            batch: Batch
+            for i, batch in enumerate(self.train_loader):
+                batch = batch.to(self.cfg.device)
+                self.optimizer.zero_grad()
+                loss = self.model.forward_batch(batch)
+                loss.backward()
+                self.optimizer.step()
+                loss = loss.item()
+
+                tb_writer.add_scalar('Train/Loss', loss, num_iter * epoch + i)
+
+                running_loss_meter.add(loss * len(batch), len(batch))
+                total_loss_meter.add(loss * len(batch), len(batch))
+
+                if (i + 1) % log_interval == 0:
+                    self.logger.info(
+                        'Epoch [%3d/%3d] - [%6.2f] Running Loss = %.4f. Total loss = %.4f.',
+                        epoch + 1,
+                        self.cfg.n_epochs,
+                        (i + 1) * 100 / num_iter,
+                        running_loss_meter.compute(),
+                        total_loss_meter.compute()
+                    )
+                    running_loss_meter.reset()
+
+                if self.is_debug and i == 2:
+                    break
+
+            train_loss = total_loss_meter.compute()
+            train_running_loss = running_loss_meter.compute()
+
+            if self.lr_scheduler is not None:
+                self.lr_scheduler.step()
+
+            torch.save(self.state_dict(epoch), ckpt_dir.joinpath('latest.pth'))
+
+            if (epoch + 1) % self.cfg.validate_steps == 0:
+                self.model.eval()
+                with torch.no_grad():
+                    self._visualize_epoch(epoch + 1)
+                    val_loss, val_metrics = self._validate_epoch(epoch + 1, tb_writer)
+                self.model.train()
+
+                if val_loss < best_loss:
+                    self.logger.info(
+                        'Found better validation loss. Improved from %.4f to %.4f',
+                        best_loss, val_loss
+                    )
+                    best_loss = val_loss
+
+                if self.cfg.save_by is None:
+                    continue
+
+                if self.cfg.save_by in val_metrics.keys():
+                    metric_val = val_metrics[self.cfg.save_by]
+                elif self.cfg.save_by == 'val_loss':
+                    metric_val = val_loss
+                elif self.cfg.save_by == 'train_loss':
+                    metric_val = train_loss
+                elif self.cfg.save_by == 'train_running_loss':
+                    metric_val = train_running_loss
+                else:
+                    raise ValueError(f'Unknow save_by={self.cfg.save_by}')
+
+                best_metric = max(metric_val, best_metric)
+                ckpt_path = ckpt_dir.joinpath(f'{self.cfg.save_by}={metric_val}.pth')
+                torch.save(self.state_dict(epoch + 1), ckpt_path)
+
+                try:
+                    ckpt_history.put_nowait(ckpt_path)
+                except queue.Full:
+                    oldest_checkpoint = ckpt_history.get()
+                    oldest_checkpoint.unlink()
+                    ckpt_history.put(ckpt_path)
+
+            if self.is_debug and (epoch + 1) == 2:
+                break
+
+        tb_writer.close()
+
+    def _visualize_epoch(self, epoch):
+        if self.is_debug:
+            num_iter = 1
+        else:
+            num_iter = _normalize_interval(self.val_loader, self.cfg.num_iter_visualize)
+        self.logger.info('Visualize Epoch [{:3d}]'.format(epoch))
+
+        batch: Batch
+        for i, batch in enumerate(self.val_loader):
+            if i == num_iter:
+                break
+            batch = batch.to(self.cfg.device)
+            indices = self.model.decode_greedy(batch.images, self.cfg.max_length, batch.image_mask)[0]
+            predict_strs = self.vocab.translate(indices, self.cfg.reduction_char_visualize)
+            for predict_str, tgt in zip(predict_strs, batch.text_str):
+                self.logger.info(f'Predict: {predict_str} ; Target: {tgt}')
+
+    def _validate_epoch(self, epoch: int, tb_writer: SummaryWriter) -> Tuple[float, Dict[str, float]]:
+        logger = logging.getLogger('Validation')
+
+        total_loss_meter = AverageMeter()
+        metrics = {
+            'CER': GlobalCERMeter(),
+            'NormCER': NormCERMeter(),
+            'WER': GlobalWERMeter(),
+            'NormWER': NormWERMeter(),
+            'ACC': ACCMeter(),
+        }  # type: Dict[str, AverageMeter]
+
+        num_iter = len(self.val_loader)
+        log_interval = _normalize_interval(self.val_loader, self.cfg.log_interval)
+
+        batch: Batch
+        for i, batch in enumerate(self.val_loader):
+            batch = batch.to(self.cfg.device)
+            loss = self.model.forward_batch(batch)
+            indices = self.model.decode_greedy(batch.images, self.cfg.max_length, batch.image_mask)[0]
+
+            pred_tokens = self.vocab.translate(indices)
+            tgt_tokens = self.vocab.translate(batch.text)
+
+            total_loss_meter.add(loss.item() * len(batch), len(batch))
+            for metric in metrics.values():
+                metric.update(pred_tokens, tgt_tokens)
+
+            if (i + 1) % log_interval == 0:
+                logger.info('Epoch [{:3d}] - [{:6.2f}] val_loss = {:.4f} - {}'.format(
+                    epoch,
+                    (i + 1) * 100 / num_iter,
+                    total_loss_meter.compute(),
+                    ' - '.join([f'{k}: {v.compute():.4f}' for k, v in metrics.items()])
+                ))
+
+            if self.is_debug and i == 2:
+                break
+
+        val_loss = total_loss_meter.compute()
+        out_metrics = {k: v.compute() for k, v in metrics.items()}
+
         tb_writer.add_scalar('Validation/Loss', val_loss, epoch)
         for k, v in out_metrics.items():
             tb_writer.add_scalar(f'Validation/{k}', v, epoch)
 
-    logger.info('Epoch [{:3d}] - val_loss = {:.4f} - {}'.format(
-        epoch,
-        val_loss,
-        ' - '.join([f'{k}: {v:.4f}' for k, v in out_metrics.items()])
-    ))
+        logger.info('Epoch [{:3d}] - val_loss = {:.4f} - {}'.format(
+            epoch,
+            val_loss,
+            ' - '.join([f'{k}: {v:.4f}' for k, v in out_metrics.items()])
+        ))
 
-    for k, v in out_metrics.items():
-        if k != 'ACC':
-            out_metrics[k] = -v
+        for k, v in out_metrics.items():
+            if k != 'ACC':
+                out_metrics[k] = -v
 
-    return val_loss, out_metrics
-
-
-def train_s2s_epoch(cfg: S2STrainConfig,
-                    epoch: int,
-                    model: ITrainableS2S,
-                    optimizer: torch.optim.Optimizer,
-                    train_loader: DataLoader,
-                    tb_writer: Optional[SummaryWriter] = None):
-    r"""
-    Training a model for one epoch
-    """
-    logger = logging.getLogger('Trainer')
-
-    total_loss = AverageMeter()
-    running_loss = AverageMeter()
-
-    num_iter = len(train_loader)
-    log_interval = cfg.log_interval
-    if isinstance(log_interval, float):
-        assert 0 <= log_interval <= 1
-        log_interval = int(log_interval * num_iter)
-
-    if not model.training:
-        model.train()
-
-    batch: Batch
-    for i, batch in enumerate(train_loader):
-        batch = batch.to(cfg.device)
-        loss = _train_s2s_iteration(model, optimizer, batch)
-
-        with torch.no_grad():
-
-            if tb_writer is not None:
-                tb_writer.add_scalar('Train/Loss', loss, num_iter * epoch + i)
-
-            running_loss.add(loss * len(batch), len(batch))
-            total_loss.add(loss * len(batch), len(batch))
-
-            if (i + 1) % log_interval == 0:
-                logger.info('Epoch [{:3d}/{:3d}] - [{:6.2f}%] Running Loss = {:.4f}. Total loss = {:.4f}.'.format(
-                    epoch + 1,
-                    cfg.n_epochs,
-                    (i + 1) * 100 / num_iter,
-                    running_loss.compute(),
-                    total_loss.compute()
-                ))
-                running_loss.reset()
-
-    return total_loss.compute(), running_loss.compute()
-
-
-def train_s2s(cfg: S2STrainConfig,
-              model: ITrainableS2S,
-              optimizer: torch.optim.Optimizer,
-              train_loader: DataLoader,
-              val_loader: DataLoader,
-              translator: ITranslator,
-              lr_scheduler: Optional[torch.optim.lr_scheduler._LRScheduler] = None,
-              is_debug: bool = False,
-              ):
-
-    def state(epoch):
-        state_dict = {}
-        state_dict['model'] = model.state_dict()
-        state_dict['optimizer'] = optimizer.state_dict()
-        if lr_scheduler is not None:
-            state_dict['lr_scheduler'] = lr_scheduler.state_dict()
-        state_dict['epoch'] = epoch
-        return state_dict
-
-    setup_logging()
-    logger = logging.getLogger('Trainer')
-
-    best_loss = float('inf')
-    best_metric = 0.0
-    model.train()
-    model.to(cfg.device)
-
-    if is_debug:
-        logger.info('Start training in debug mode')
-        logger.info('Run training for 1 iteration')
-        batch = next(iter(train_loader))
-        batch = batch.to(cfg.device)
-        _train_s2s_iteration(model, optimizer, batch)
-
-        if lr_scheduler is not None:
-            lr_scheduler.step()
-
-        model.eval()
-        logger.info('Run validation for 1 iteration')
-        batch = next(iter(val_loader))
-        batch = batch.to(cfg.device)
-        model.forward_batch(batch)
-        model.decode_greedy(batch.images, cfg.max_length, batch.image_mask)
-        logger.info('Training DONE')
-        return
-
-    session_dir = Path(create_session_dir(cfg.log_dir))
-
-    tensorboard_dir = session_dir.joinpath('tb_logs')
-    tb_writer = SummaryWriter(tensorboard_dir)
-
-    ckpt_dir = session_dir.joinpath('ckpt')
-    ckpt_dir.mkdir(parents=True)
-    ckpt_history: queue.Queue = queue.Queue(cfg.save_top_k)
-
-    logger.info('Start training')
-
-    for epoch in range(cfg.n_epochs):
-        train_loss, train_running_loss = train_s2s_epoch(cfg, epoch, model, optimizer, train_loader, tb_writer)
-
-        if lr_scheduler is not None:
-            lr_scheduler.step()
-
-        torch.save(state(epoch), ckpt_dir.joinpath('latest.pth'))
-
-        if (epoch + 1) % cfg.validate_steps == 0:
-            model.eval()
-            with torch.no_grad():
-                visualize_s2s(cfg, epoch + 1, model, translator, val_loader)
-                val_loss, val_metrics = validate_s2s(cfg, epoch + 1, model, translator, val_loader, tb_writer)
-            model.train()
-
-            if val_loss < best_loss:
-                logger.info('Found better validation loss. Improved from {:.4f} to {:.4f}'.format(
-                    best_loss, val_loss
-                ))
-                best_loss = val_loss
-
-            if cfg.save_by is None:
-                continue
-
-            if cfg.save_by in val_metrics.keys():
-                metric_val = val_metrics[cfg.save_by]
-            elif cfg.save_by == 'val_loss':
-                metric_val = val_loss
-            elif cfg.save_by == 'train_loss':
-                metric_val = train_loss
-            elif cfg.save_by == 'train_running_loss':
-                metric_val = train_running_loss
-            else:
-                raise ValueError(f'Unknow save_by={cfg.save_by}')
-
-            best_metric = max(metric_val, best_metric)
-            ckpt_path = ckpt_dir.joinpath(f'{cfg.save_by}={metric_val}.pth')
-            torch.save(state(epoch + 1), ckpt_path)
-
-            try:
-                ckpt_history.put_nowait(ckpt_path)
-            except queue.Full:
-                oldest_checkpoint = ckpt_history.get()
-                oldest_checkpoint.unlink()
-                ckpt_history.put(ckpt_path)
-
-    tb_writer.close()
+        return val_loss, out_metrics
 
 
 def setup_logging():
@@ -317,3 +272,13 @@ def create_session_dir(root_dir: str, name: Optional[str] = None, exist_ok: bool
     log_dir = Path(root_dir, name)
     log_dir.mkdir(parents=True, exist_ok=exist_ok)
     return str(log_dir)
+
+
+def _normalize_interval(loader: DataLoader, interval: Union[int, float]) -> int:
+    num_iter = len(loader)
+    interval = interval
+    if isinstance(interval, float):
+        assert 0 <= interval <= 1
+        interval = int(interval * num_iter)
+
+    return interval
