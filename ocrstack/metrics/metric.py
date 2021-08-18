ce7a9106458c512e6108fd09e1dae928fd4e239c
Loi Ly <vinhloiit1327@gmail.com>
2021-08-18 12:06:40 +0700

    add typehint for average meter

--- ocrstack/metrics/metric.py
+++ ocrstack/metrics/metric.py
@@ -9,16 +9,16 @@ class AverageMeter(object):
         self._sum = 0
         self._num_samples = 0
 
-    def compute(self):
+    def compute(self) -> float:
         if self._num_samples == 0:
             return 0.
         return self._sum / self._num_samples
 
-    def reset(self):
+    def reset(self) -> None:
         self._sum = 0
         self._num_samples = 0
 
-    def add(self, sum, n_samples):
+    def add(self, sum, n_samples) -> None:
         self._sum += sum
         self._num_samples += n_samples
 
