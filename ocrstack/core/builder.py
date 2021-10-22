ac80a20ca9a0185bba141ea2cdfd617f49ba30a4
Loi Ly <vinhloiit1327@gmail.com>
2021-10-22 14:11:43 +0700

    add predefined registries

--- ocrstack/core/builder.py
+++ ocrstack/core/builder.py
@@ -4,7 +4,29 @@ LOSS_REGISTRY = Registry('Loss')
 BACKBONE_REGISTRY = Registry('Backbone')
 ENCODER_REGISTRY = Registry('Encoder')
 DECODER_REGISTRY = Registry('Decoder')
+EMBEDDING_REGISTRY = Registry('Embedding')
+MODEL_REGISTRY = Registry('Model')
 
 
 def build_backbone(cfg):
     return BACKBONE_REGISTRY.build_from_cfg(cfg)
+
+
+def build_encoder(cfg):
+    return ENCODER_REGISTRY.build_from_cfg(cfg)
+
+
+def build_decoder(cfg):
+    return DECODER_REGISTRY.build_from_cfg(cfg)
+
+
+def build_loss(cfg):
+    return LOSS_REGISTRY.build_from_cfg(cfg)
+
+
+def build_embedding(cfg):
+    return EMBEDDING_REGISTRY.build_from_cfg(cfg)
+
+
+def build_model(cfg):
+    return MODEL_REGISTRY.build_from_cfg(cfg)
