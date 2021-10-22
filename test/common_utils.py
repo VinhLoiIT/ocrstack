48fdfd71be1d3c89fd2f6449312257363197ca5d
Loi Ly <vinhloiit1327@gmail.com>
2021-10-22 14:46:13 +0700

    add some test for builders

---
+++ test/common_utils.py
@@ -0,0 +1,33 @@
+import torch
+
+
+def _create_dummy_image(batch_size: int = 1,
+                        channels: int = 3,
+                        height: int = 64,
+                        width: int = 64,
+                        dtype=torch.float,
+                        device=torch.device('cpu')):
+    if dtype == torch.uint8:
+        return torch.randint(0, 256, (batch_size, channels, height, width), dtype=dtype, device=device)
+
+    return torch.rand(batch_size, channels, height, width, dtype=dtype, device=device)
+
+
+def _create_dummy_sequence(batch_size: int = 1,
+                          length: int = 10,
+                          channels: int = 5,
+                          dtype=torch.float,
+                          device=torch.device('cpu')):
+    return torch.rand((batch_size, length, channels), dtype=dtype, device=device)
+
+
+def _create_dummy_sequence_indices(batch_size: int = 1,
+                                  length: int = 10,
+                                  max_index: int = 5,
+                                  one_hot: bool = False,
+                                  device=torch.device('cpu')):
+    dummy = torch.randint(0, max_index, (batch_size, length), device=device)
+    dummy_onehot = None
+    if one_hot:
+        dummy_onehot = F.one_hot(dummy, max_index)
+    return dummy, dummy_onehot
